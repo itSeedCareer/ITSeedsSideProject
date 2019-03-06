f09a33e8eea4b972b201896df918fa85dd379098
Troy_Hung <b04303032@ntu.edu.tw>
2019-03-07 02:48:12 +0800

    try login page

--- ITSeedsSideProject/urls.py
+++ ITSeedsSideProject/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from mainsite.views import homepage
+from mainsite.views import homepage, showpost
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage),
+    path('post/<slug:slug>', showpost),
 ]
 
 # urlpatterns = [
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Post
+from datetime import datetime
 
 # Create your views here.
 def homepage(request):
     posts = Post.objects.all()
-    post_lists = list()
-    for count, post in enumerate(posts):
-        post_lists.append("No. {} : ".format(str(count)) + str(post) + "<hr>")
-        post_lists.append("<small>" + str(post.body.encode('utf8')) \
-                          + "</small><br><br>")
-    return HttpResponse(post_lists)
+    now = datetime.now()
+    return render(request, 'index.html', locals())
+
+def showpost(request, slug):
+    try:
+        post = Post.objects.get(slug = slug)
+        if post != None:
+            return render(request, 'post.html', locals())
+    except:
+        return redirect('/')
