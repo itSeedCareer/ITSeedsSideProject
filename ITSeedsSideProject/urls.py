6d2e960291448951795580f33f8edfbbc22ed570
Troy_Hung <b04303032@ntu.edu.tw>
2019-03-06 22:41:55 +0800

    try add some article

--- ITSeedsSideProject/urls.py
+++ ITSeedsSideProject/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url
+from django.urls import path, include
+from mainsite.views import homepage
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homepage),
 ]
 
 # urlpatterns = [
