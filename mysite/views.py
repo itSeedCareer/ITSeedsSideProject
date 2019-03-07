a531e9904f868e84edcc8ca47536ebed09a5f62e
Troy_Hung <b04303032@ntu.edu.tw>
2019-03-07 17:04:09 +0800

    finish login page and add templates

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from mysite import  models
 
 # Create your views here.
+
+def index(request):
+    try:
+        urId = request.GET['userId']
+        urPassword = request.GET['userPassword']
+    except:
+        urId = None
+
+    if urId != None and urPassword == '12345':
+        verified = True
+    else:
+        verified = False
+    return render(request, 'index.html', locals())
