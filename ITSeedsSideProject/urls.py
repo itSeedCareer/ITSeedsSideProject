721cd26c2663a2debf32c0bfb3aa060e52a9e7b7
Troy_Hung <b04303032@ntu.edu.tw>
2019-03-11 18:02:57 +0800

    100 % signUp page

--- ITSeedsSideProject/urls.py
+++ ITSeedsSideProject/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from mainsite.views import homepage, showpost
 from mysite.views import index, signUp
 
 
